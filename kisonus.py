914438458b10f5e71dcd0acd056a15a651383656
bukseng <glennabalos.mark@gmail.com>
2023-05-23 07:36:34 +0800

    first commit

---
+++ kisonus.py
@@ -0,0 +1,67 @@
+import tkinter
+import tkinter.font as tkFont
+from tkinter import *
+import keyboard
+import pygame
+
+
+audio_map = {
+    'None': 'None',
+    'Mechanical A': 'res/mech_00.wav',
+    'Mechanical B': 'res/mech_01.wav',
+    'Mechanical C': 'res/mech_02.wav',
+    'Mechanical D': 'res/mech_03.wav',
+    'Mechanical E': 'res/mech_04.wav',
+    'Mechanical F': 'res/mech_05.wav',
+    'Mechanical G': 'res/wood_00.wav',
+    'Vintage A': 'res/norm_00.wav',
+    'Vintage B': 'res/norm_01.wav',
+    'Vintage C': 'res/norm_02.wav',
+    'Typewriter A': 'res/typw_00.wav',
+    'Typewriter B': 'res/typw_01.wav',
+    'Typewriter C': 'res/silent_00.wav',
+    'Laptop A': 'res/laptop_00.wav',
+    'Laptop B': 'res/laptop_01.wav'
+}
+
+def play_sound(audio_file, volume=1.0):
+    pygame.mixer.init()
+    audio = pygame.mixer.Sound(audio_file)
+    audio.set_volume(volume)
+    audio.play()
+
+
+def on_key_press(event):
+    if current_sound != 'None':
+        play_sound(current_sound)
+        
+  
+def changeSound(event):
+    global current_sound
+    current_sound = audio_map[chosen.get()]            
+
+current_sound = 'None'
+
+keyboard.on_press(on_key_press)
+
+
+root = Tk()
+root.geometry('300x30')
+root.title('Kisonus')
+root.iconbitmap('res/kisonus_icon.ico')
+root.resizable(0,0)
+
+
+options = [
+    'None', 'Mechanical A','Mechanical B','Mechanical C','Mechanical D','Mechanical E','Mechanical F','Mechanical G',
+    'Vintage A', 'Vintage B', 'Vintage C', 'Typewriter A', 'Typewriter B', 'Typewriter C', 'Laptop A', 'Laptop B'
+]
+
+chosen = StringVar()
+chosen.set('None')    
+  
+drop = OptionMenu( root, chosen, *options, command=changeSound)
+drop.config(font=tkFont.Font(family='Calibri', size=16))
+drop.pack()
+
+root.mainloop()
